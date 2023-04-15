63812e1d1d2a82e1abdd33eb586d85c7ada0de9c
DavideProgrammer <127288668+DavideProgrammer@users.noreply.github.com>
2023-04-15 11:58:29 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
 function turn_right () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 85)
-    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 85)
-    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 60)
-    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 60)
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 15)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 15)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 22)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 22)
 }
 function car_forward () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 40)
-    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 40)
+    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 15)
+    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 15)
+    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 15)
+    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 15)
 }
 function car_downward () {
-	
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 5)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 5)
+    mecanumRobot.Motor(LR.Upper_right, MD.Back, 5)
+    mecanumRobot.Motor(LR.Lower_right, MD.Back, 5)
 }
 function turn_left () {
-    mecanumRobot.Motor(LR.Upper_left, MD.Forward, 60)
-    mecanumRobot.Motor(LR.Lower_left, MD.Forward, 60)
-    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 85)
-    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 85)
+    mecanumRobot.Motor(LR.Upper_left, MD.Back, 22)
+    mecanumRobot.Motor(LR.Lower_left, MD.Back, 22)
+    mecanumRobot.Motor(LR.Upper_right, MD.Forward, 15)
+    mecanumRobot.Motor(LR.Lower_right, MD.Forward, 15)
 }
 basic.showIcon(IconNames.TShirt)
 basic.forever(function () {
@@ -26,7 +29,7 @@ basic.forever(function () {
     } else if (mecanumRobot.LineTracking(LT.Left) == 1 && mecanumRobot.LineTracking(LT.Right) == 0) {
         turn_left()
     } else if (mecanumRobot.LineTracking(LT.Left) == 0 && mecanumRobot.LineTracking(LT.Right) == 0) {
-        mecanumRobot.state(MotorState.stop)
+        car_downward()
     } else {
         car_forward()
     }
